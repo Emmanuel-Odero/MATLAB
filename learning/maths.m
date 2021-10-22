7551167f629ef55a5f15e22f5c634fb1a5b152cf
unknown <emmanuelodero808@gmail.com>
2021-10-22 12:34:16 +0300

    comment

--- learning/maths.m
+++ learning/maths.m
@@ -3,3 +3,5 @@ Using matlab as calculator
 ---Matlab can also be used a mathematical calculator for solving simple to complex mathematical problems.
 
 %}
+%to find the square root of a number on matlab we use the sqrt() method.
+sqrt(49) %the answer will be 7

93cf85d686b71f9ba3efb4e6de43dee5560ed11b
unknown <emmanuelodero808@gmail.com>
2021-10-22 12:24:03 +0300

    changes made to the learning file

---
+++ learning/maths.m
@@ -0,0 +1,5 @@
+%{
+Using matlab as calculator
+---Matlab can also be used a mathematical calculator for solving simple to complex mathematical problems.
+
+%}
---
+++ learning/variable.m
@@ -0,0 +1,23 @@
+%This file teaches variables in matlab and how to declare them
+%{
+In Matlab every variable is an array or matrix
+It is important to note that −
+
+-----------Once a variable is entered into the system, you can refer to it later.
+
+-----------Variables must have values before they are used.
+
+-----------When an expression returns a result that is not assigned to any variable,
+           the system assigns it to a variable named ans, which can be used later
+ %}
+
+
+  % the below line of code declare variable name assigns it John Doe and the variable name is later on diplayed to the screen
+  name = "John Doe";
+  disp(name)
+x = 3	       % defining x and initializing it with a value
+
+%{
+Combining Variables (Strings)
+---The additional operator "+" can be used to add variables in MATLAB
+%}

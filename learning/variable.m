da5d87b617eb052e0b8e34c63bbe5b5ae0c7cbba
Emmanuel-Odero <65104775+Emmanuel-Odero@users.noreply.github.com>
2021-11-03 21:45:21 +0300

    Add files via upload

---
+++ learning/variable.m
@@ -0,0 +1,32 @@
+%This file teaches variables in matlab and how to declare them
+%{
+In Matlab every variable is an array or matrix
+It is important to note that −
+
+-----------Once a variable is entered into the system, you can refer to it later.
+
+-----------Variables must have values before they are used.
+
+-----------When an expression returns a result that is not assigned to any variable,
+           the system assigns it to a variable named ans, which can be used later
+ %}
+
+
+  % the below line of code declare variable name assigns it John Doe and the variable name is later on diplayed to the screen
+  name = "John Doe";
+  disp(name)
+x = 3	       % defining x and initializing it with a value
+
+%{
+Combining Variables (Strings)
+---The additional operator "+" can be used to add variables in MATLAB
+
+The whos command displays little more about the variables −
+---who
+
+Variables currently stored on the memory
+Type of each variables
+Memory allocated to each variable
+Whether they are complex variables or not
+%}
+

2d6072bcb3208eb3418043d99556b7e0e2df8fd5
Emmanuel-Odero <65104775+Emmanuel-Odero@users.noreply.github.com>
2021-11-03 21:43:45 +0300

    Create maths.m

---
+++ maths.m
@@ -0,0 +1,15 @@
+%{
+Using matlab as calculator
+---Matlab can also be used a mathematical calculator for solving simple to complex mathematical problems.
+
+%}
+%to find the square root of a number on matlab we use the sqrt() method.
+sqrt(49) %the answer will be 7
+%carrying out addition we use the add/plus "+" opperator
+
+%Having multiple Assignnments --it is very much possible to have multiple Assignnments on one line on Matlab
+
+a = 2; b = 7; c = a * b
+
+%The 'who' command is used to display all the variables used
+
